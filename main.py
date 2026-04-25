b08b1551c699801a43464d5cbc654278c863b319
Aryan <aryan@gupta-inc.com>
2026-04-25 14:24:30 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# image processing 
+import numpy as np
+from PIL import Image
+import math
+
+
+
+
+
+
+
+
+
+
+def main():
+    print("Hello from quadtree-image!")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
